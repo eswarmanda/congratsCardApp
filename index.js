e7df624d92c33ec14bc30c44b1cbbadfe024aae3
eswarmanda <eswar95manda@gmail.com>
2023-07-09 14:20:14 +0000

    congratsCardApp

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div>
+    <h1 className="heading">Congratulations</h1>
+
+    <div className="card">
+      <img
+        class=""
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        alt=""
+      />
+      <h2>Kiran V</h2>
+      <p>Vishnu education of Computer education and technology Bhimavaram</p>
+      <img
+        class=""
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        alt=""
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
